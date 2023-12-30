a002fa4b6887d9ac2ce568ad8d83b5bb2553d1a8
nguyenxuantruc <xtruc.tt@gmail.com>
2023-12-30 14:00:18 +1100

    add install docker script

---
+++ install_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install nano glances -y
+
+###DOCKER###
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+#Install Docker Engine
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# After installation, check that Docker Engine is running:
+sudo groupadd docker
+sudo usermod -aG docker $USER
