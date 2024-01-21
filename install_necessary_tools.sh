d5d36f33ff74c35aa2649dd4146a6235bda89a60
nguyenxuantruc <xtruc.tt@gmail.com>
2024-01-21 10:45:58 +0700

    rename

--- install_necessary_tools.sh
+++ install_necessary_tools.sh
@@ -24,3 +24,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # After installation, check that Docker Engine is running:
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+#Install psql
+sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install postgresql
